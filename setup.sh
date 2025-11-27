63031d922589a30afa6e7cb96c5b8a436df1a54d
Akshay Krishna <dev@akshikrm.com>
2025-11-27 05:30:59 +0000

    Update Neovim setup script for new directory and plugins

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-NVIM_DIR="$HOME/.config/nvim-test"
-PACK_DIR="$NVIM_DIR/pack/akshikrm/start"
+NVIM_DIR="$HOME/.config/nvim-rewrite"
+PACK_DIR="$NVIM_DIR/pack/plugins/start"
 
 echo "Creating Neovim directory structure..."
 mkdir -p "$PACK_DIR"
@@ -11,22 +11,24 @@ mkdir -p "$PACK_DIR"
 cd "$PACK_DIR"
 
 clone() {
-	local repo=$1
-	local name=$(basename "$repo")
+    local repo=$1
+    local name=$(basename "$repo" .git)
 
-	if [ -d "$name" ]; then
-		echo "$name already exists — skipping"
-	else
-		echo "Cloning $repo..."
-		git clone --depth 1 "$repo"
-	fi
+    if [ -d "$name" ]; then
+        echo "$name already exists — skipping"
+    else
+        echo "Cloning $repo..."
+        git clone --depth 1 "$repo"
+    fi
 }
 
-clone https://github.com/saghen/blink.cmp
+clone https://github.com/numToStr/Comment.nvim
 clone https://github.com/stevearc/conform.nvim
+clone https://github.com/neovim/neovim
 clone https://github.com/neovim/nvim-lspconfig
 clone https://github.com/kylechui/nvim-surround
 clone https://github.com/nvim-treesitter/nvim-treesitter
+clone https://github.com/JoosepAlviste/nvim-ts-context-commentstring
 clone https://github.com/christoomey/vim-tmux-navigator
 
 echo "Neovim package setup complete!"
